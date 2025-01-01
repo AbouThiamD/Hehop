8c0cb49bb5264c099ce6e96084c92c1ec920309d
abou <abouthiam00123@gmail.com>
2025-01-01 23:53:22 +0100

    Premier commit EHOP

---
+++ portofolio.js
@@ -0,0 +1,12 @@
+const sr = ScrollReveal({
+    distance:'65px',
+    duration:2600,
+    delay:450,
+    reset: true
+});
+
+sr.reveal('.img-item',{delay:200, origin:'top'});
+sr.reveal('.home-info',{delay:200, origin:'top'});
+
+
+
